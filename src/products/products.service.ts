5358b638c66acfdbc468b80598a2fd71d7e3ee6e
Andres meza <amezadeveloper@gmail.com>
2025-01-05 09:03:10 -0500

    cambio en readme

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,9 @@
-
-
-
-import { PrismaClient } from '@prisma/client';
-   
-  
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 
 import { CreateProductDto } from './dto/create-product.dto';
 import { PaginationDto } from 'src/common';
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
-
+import { PrismaClient } from '@prisma/client';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
